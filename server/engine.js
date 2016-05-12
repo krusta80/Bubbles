744fb76adfa0d5da466cbb94a6f41c77e45ec0b8
John Gruska <jag47@cornell.edu>
2016-05-11 23:43:54 -0400

    Broke up building-block functions into functions.js and created engine.js (including an updateState function)

--- server/engine.js
+++ server/engine.js
@@ -1,106 +1,54 @@
-const FPS = 30;									
+var gameFunctions = require('./functions');
 
-const STARTING_RADIUS = 1;							//	size of a newly-spawned bubble
-const STARTING_MAX_SPEED = STARTING_RADIUS*5;		//	max speed of a newly-spawned bubble
-
-var playerBubble = {name: "hero", radius: 20, x: 5, y: 5};
-var bubble2 = {name: "villain", radius: 12, x: 10, y: 5};
-
-
-/**
-	The following functions are used to identify and handle
-	bubble collisions.
-*/
-
-var haveCollided = function(bubble1, bubble2) {
-	return getDistance(bubble1, bubble2) < bubble1.radius + bubble2.radius;
+var Engine = function(framesPerSecond, width, height, maxBubbles) {
+	this.FPS = framesPerSecond;
+	this.width = width;
+	this.height = height;
+	this.maxBubbles = maxBubbles;
+	this.bubbles = {};
+	this.bubbleKeys = [];
+	this.pellets = {};
+	this.frame = 0;
 };
 
-var getPostGobbleRadius = function(bubble1, bubble2) {
-	//	The larger of the two (of course)
-	var radius1 = bubble1.radius;
-	var radius2 = bubble2.radius;
-	return Math.sqrt(radius1*radius1 + radius2*radius2);
-};
-
-var getCollisionResult = function(bubble1, bubble2) {
-	if(bubble1.radius === bubble2.radius)
-		return {winner: undefined, loser: undefined};
-	else if(bubble1.radius > bubble2.radius)  
-		return {winner: bubble1, loser: bubble2};
-	else
-		return {winner: bubble2, loser: bubble1};
-};
-
-
-/**
-	The following functions are used to determine a player's 
-	speed and bearing.
-*/
-
-var getPlayerVector = function(bubble, mouseDx, mouseDy) {
-	//	mouseDx and mouseDy are assumed to be measured in pixels relative to 
-	//	the center of the player's bubble, which is assigned coordinates (0,0)
+Engine.prototype.updateState = function() {
+	//	We go through all bubbles and process their latest 
+	//  vector info to update their positions
+	var bubble, bubble2;
+	var tmp; 
+	var bubbleLost = false;
 	
-	var mouseDistance = Math.sqrt(mouseDx*mouseDx + mouseDy*mouseDy);
-	var playerSpeed = getPlayerSpeed(bubble, mouseDistance);
+	//	first we update the positions 						O(n)
+	for(var i = 0; i < this.bubbleKeys.length; i++) {
+		bubble = this.bubbles[this.bubbleKeys[i]];
+		if(bubble.vector) {
+			tmp = bubble.x + bubble.vector.dx;
+			if(tmp >= 0 && tmp < this.width)
+				bubble.x = tmp;
+			tmp = bubble.y + bubble.vector.dy;
+			if(tmp >= 0 && tmp < this.height)
+				bubble.y = tmp;
+			delete bubble.vector;
+		}
+	}
 
-	return {
-		dx: playerSpeed * mouseDx / mouseDistance,
-		dy: playerSpeed * mouseDx / mouseDistance
-	};
+	//	next we check for collisions 						O(n^2)
+	for(var i = 0; i < this.bubbleKeys.length; i++) {
+		bubble = this.bubbles[this.bubbleKeys[i]];
+		for(var j = i+1; j < this.bubbleKeys.length; j++) {
+			bubble2 = this.bubbles[this.bubbleKeys[j]];
+			if(gameFunctions.haveCollided(bubble, bubble2)) {
+				tmp = gameFunctions.getCollisionResult(bubble, bubble2);
+				if(tmp.winner) {
+					tmp.winner.radius = gameFunctions.getPostGobbleRadius(bubble, bubble2);
+					delete this.bubbles[tmp.loser.name];
+					bubbleLost = true;
+				}
+			}
+		}
+	}
+
+	//	reset bubbleKeys
+	if(bubbleLost)
+		this.bubbleKeys = Object.keys(this.bubbles);
 };
-
-var getPlayerSpeed = function(bubble, mouseDistance) {
-	return getMaxSpeed(bubble.radius) * getSpeedFraction(mouseDistance);
-};
-
-var getMaxSpeed = function(radius) {
-	return STARTING_MAX_SPEED/Math.sqrt(radius);
-};
-
-var getSpeedFraction = function(distanceFromCenter) {
-	//	assuming distanceFromCenter measured in STARTING_RADIUS increments
-	var pers = [0, .1, .4, .8, 1];
-	var floored = Math.floor(distanceFromCenter);
-	if(floored > 4)
-		floored = 4;
-	return pers[floored];
-};
-
-
-/**
-	The following functions are used to measure two objects'
-	relative distance.
-*/
-
-var getDistanceAndComponents = function(object1, object2) {
-	//	assumes that passed object will have properties x and y
-	//  and is from object1's perspective
-	return {
-		dx: getDx(object1, object2),
-		dy: getDy(object1, object2),
-		distance: getDistance(object1, object2)
-	};
-};
-
-var getDistance = function(object1, object2) {
-	var dx = getDx(object1, object2);
-	var dy = getDy(object1, object2);
-	return Math.sqrt(dx*dx + dy*dy);
-};
-
-var getDx = function(object1, object2) {
-	return object2.x - object1.x;
-};
-
-var getDy = function(object1, object2) {
-	return object2.y - object1.y;
-};
-
-console.log("Player Bubble   :", playerBubble);
-console.log("Bubble 2        :", bubble2);
-console.log("Collision?      :", haveCollided(playerBubble, bubble2));
-console.log("Winner          :", getCollisionResult(playerBubble, bubble2).winner.name);
-console.log("Winner's Radius :", getPostGobbleRadius(playerBubble, bubble2));
-
---
+++ server/functions.js
@@ -0,0 +1,109 @@
+
+/**
+	The following functions are used to identify and handle
+	bubble collisions.
+*/
+
+module.exports = {
+
+	STARTING_RADIUS: 1,							//	size of a newly-spawned bubble
+	STARTING_MAX_SPEED: this.STARTING_RADIUS*5,		//	max speed of a newly-spawned bubble
+	
+	haveCollided: function(bubble1, bubble2) {
+		return this.getDistance(bubble1, bubble2) < bubble1.radius + bubble2.radius;
+	},
+
+	getPostGobbleRadius: function(bubble1, bubble2) {
+		//	The larger of the two (of course)
+		var radius1 = bubble1.radius;
+		var radius2 = bubble2.radius;
+		return Math.sqrt(radius1*radius1 + radius2*radius2);
+	},
+
+	getCollisionResult: function(bubble1, bubble2) {
+		if(bubble1.radius === bubble2.radius)
+			return {winner: undefined, loser: undefined};
+		else if(bubble1.radius > bubble2.radius)  
+			return {winner: bubble1, loser: bubble2};
+		else
+			return {winner: bubble2, loser: bubble1};
+	},
+
+
+	/**
+		The following functions are used to determine a player's 
+		speed and bearing.
+	*/
+
+	getPlayerVector: function(bubble, mouseDx, mouseDy) {
+		//	mouseDx and mouseDy are assumed to be measured in pixels relative to 
+		//	the center of the player's bubble, which is assigned coordinates (0,0)
+		
+		var mouseDistance = Math.sqrt(mouseDx*mouseDx + mouseDy*mouseDy);
+		var playerSpeed = this.getPlayerSpeed(bubble, mouseDistance);
+
+		return {
+			dx: playerSpeed * mouseDx / mouseDistance,
+			dy: playerSpeed * mouseDx / mouseDistance
+		};
+	},
+
+	getPlayerSpeed: function(bubble, mouseDistance) {
+		return this.getMaxSpeed(bubble.radius) * this.getSpeedFraction(mouseDistance);
+	},
+
+	getMaxSpeed: function(radius) {
+		return this.STARTING_MAX_SPEED/Math.sqrt(radius);
+	},
+
+	getSpeedFraction: function(distanceFromCenter) {
+		//	assuming distanceFromCenter measured in STARTING_RADIUS increments
+		var pers = [0, .1, .4, .8, 1];
+		var floored = Math.floor(distanceFromCenter);
+		if(floored > 4)
+			floored = 4;
+		return pers[floored];
+	},
+
+
+	/**
+		The following functions are used to measure two objects'
+		relative distance.
+	*/
+
+	getDistanceAndComponents: function(object1, object2) {
+		//	assumes that passed object will have properties x and y
+		//  and is from object1's perspective
+		return {
+			dx: this.getDx(object1, object2),
+			dy: this.getDy(object1, object2),
+			distance: this.getDistance(object1, object2)
+		};
+	},
+	
+	getDistance: function(object1, object2) {
+		var dx = this.getDx(object1, object2);
+		var dy = this.getDy(object1, object2);
+		return Math.sqrt(dx*dx + dy*dy);
+	},
+
+	getDx: function(object1, object2) {
+		return object2.x - object1.x;
+	},
+
+	getDy: function(object1, object2) {
+		return object2.y - object1.y;
+	}
+};
+
+var testIt = function() {
+	var playerBubble = {name: "hero", radius: 20, x: 5, y: 5};
+	var bubble2 = {name: "villain", radius: 12, x: 10, y: 5};
+
+	console.log("Player Bubble   :", playerBubble);
+	console.log("Bubble 2        :", bubble2);
+	console.log("Collision?      :", module.exports.haveCollided(playerBubble, bubble2));
+	console.log("Winner          :", module.exports.getCollisionResult(playerBubble, bubble2).winner.name);
+	console.log("Winner's Radius :", module.exports.getPostGobbleRadius(playerBubble, bubble2));
+};
+
